1d22e026935d1e74099b898785e450e4f05f368b
Beverly James <jbevk94@gmail.com>
2020-08-21 10:04:20 -0700

    Completed functional calculator. Needs further styling and responsiveness.

--- script.js
+++ script.js
@@ -24,6 +24,10 @@ function printOutput(num) {
 // format the number with commas
 
 function getFormattedNumber(num) {
+    if(num == "-"){ // Allow negative numbers
+        return "";
+    }
+    
   let n = Number(num);
   let value = n.toLocaleString("en");
   return value;
@@ -38,6 +42,56 @@ function reverseNumberFormat(num){
 let operator = document.getElementsByClassName("operator");
 for(let i = 0; i<operator.length; i++){
     operator[i].addEventListener('click', function(){
-        alert("Clicked" +this.id)
+        if(this.id=="clear"){ //check if id is clear 
+            printHistory("");
+            printOutput("")
+        }
+        else if(this.id=="backspace"){ // remove commas from output by converting it to a string
+            let output=reverseNumberFormat(getOutput()).toString();
+            if(output){//if output has a value
+                output = output.substr(0,output.length-1);
+                printOutput(output);
+            }
+        } else{
+            //declare values
+            let output = getOutput();
+            let history = getHistory();
+            if (output==""&&history !=""){ //replace operator with last clicked
+                if(isNaN(history[getHistory.length-1])){//check if clicked is operator
+                    history = history.substr (0,history.length-1)//remove last character
+                }
+            }
+            if(output !="" || history != ""){//check if output or history are empty
+                output = output ==""? output: reverseNumberFormat(output);
+                // Add output to history value
+                history = history + output;
+                if(this.id=="="){
+                    let result = eval(history);
+                    printOutput(result);
+                    printHistory("")
+                }
+                else{
+                    // Other operators get added to history & output is set to empty
+                    history=history+this.id;
+                    printHistory(history);
+                    printOutput("")
+                }
+            }
+        }
+    })
+}
+
+// make numbers functional with click events
+let number = document.getElementsByClassName("number");
+for(let i = 0; i<number.length; i++){
+    number[i].addEventListener('click', function(){
+    // remove commas on output
+    let output = reverseNumberFormat(getOutput());
+    if (output !=NaN){ //if output is a number
+        output=output+this.id;
+        printOutput(output)
+
+    }
+    
     })
 }
